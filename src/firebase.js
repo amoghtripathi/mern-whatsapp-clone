9dadb4645e7408ca8ce112140fa539c3d6e2b5f5
amogh <amoghtripathi@gmail.com>
2020-09-07 04:51:12 +0530

    starting point

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7HJCWEFIMLwG-UOzqEP1Kf_I1XQQbdDo",
+    authDomain: "mern-whatsapp-273e4.firebaseapp.com",
+    databaseURL: "https://mern-whatsapp-273e4.firebaseio.com",
+    projectId: "mern-whatsapp-273e4",
+    storageBucket: "mern-whatsapp-273e4.appspot.com",
+    messagingSenderId: "66162213744",
+    appId: "1:66162213744:web:29073b44093e1378e985b5",
+    measurementId: "G-LPZQ2KE09D"
+  };
