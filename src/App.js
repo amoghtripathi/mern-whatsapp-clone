3113c11b2bd44a55775f5ec930d7076e57059b40
amogh <amoghtripathi@gmail.com>
2020-09-07 05:37:08 +0530

    basic app skeleton done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 function App() {
   return (
-    <div className="App">
-      
-        <h1>app spinup</h1>
-      
+    <div className="app">
+      <div className="app__body">
+          {/* sidebar */}
+          <Sidebar />
+        
+
+        {/* main chat window */}
+          <Chat />
+
+
+      </div>      
     </div>
   );
 }
