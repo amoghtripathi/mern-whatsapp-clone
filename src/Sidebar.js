71a802220447249160877e8dd1edd9b4b7f07aa0
amogh <amoghtripathi@gmail.com>
2020-09-07 23:31:35 +0530

    day one finalo push

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {IconButton, Avatar} from "@material-ui/core";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            hello from sidebar
+            
             <div className="sidebar__header">
+                <Avatar src="https://avatars3.githubusercontent.com/u/26096335?s
+                =460&u=cc8ea914cd668125894d14186675241e046e7996&v=4" />
 
                 <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                     
                 </div>
             </div>
