8e5b2573cfd4f89aacf83dca1973015467be2b7f
amogh <amoghtripathi@gmail.com>
2020-09-09 04:51:12 +0530

    main chat styling+ added colors n bg

--- src/Chat.js
+++ src/Chat.js
@@ -25,6 +25,36 @@ function Chat() {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Amogh</span>
+                    this is a message
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Amogh</span>
+                    and this is test message
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">Amogh</span>
+                    task failed successfully
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
         </div>
     )
 }
