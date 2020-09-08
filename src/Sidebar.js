d32fc26e4ceef257a57a955ae24f76df8ef9723d
amogh <amoghtripathi@gmail.com>
2020-09-08 05:39:18 +0530

    more sidebar styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import {IconButton, Avatar} from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
     return (
@@ -24,6 +25,12 @@ function Sidebar() {
                         <MoreVertIcon />
                     </IconButton>
                     
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="search/start new convo" type="text"/>
                 </div>
             </div>
         </div>
